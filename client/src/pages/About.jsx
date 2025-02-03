ab47dfdfc3687341a1b28ca1eb1b8febe61b21fb
Al-Abir <sheikhabir196@gmail.com>
2025-02-03 19:00:19 +0600

    about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,9 +4,10 @@ import BioData from "../components/BioData";
 const About = () => {
     return (
         <Layout>
-            <div className="container mx-auto flex gap-8">
-                {/* Left Section - BioData */}
-                <div className="w-2/4 border border-spacing-1">
+            {/* Hero Section Container */}
+            <div className="container mx-auto flex flex-col lg:flex-col md:flex-col xl:flex-row gap-8">
+                {/* Left Section - BioData (1/4 on xl, full width on others) */}
+                <div className="w-full xl:w-1/4 border border-gray-300 p-4">
                     <ul className="grid grid-cols-6 gap-8 grid-rows-6 grid-flow-row-dense">
                         <li className="xl:w-[370px] xl:h-[736px] lg:w-[280px] lg:h-[650px]  
                             md:w-[850px] md:h-[700px] w-[360px] h-[700px] mx-auto bg-[#1D1C22] 
@@ -21,8 +22,8 @@ const About = () => {
                     </ul>
                 </div>
 
-                {/* Right Section - Text */}
-                <div className="w-3/4 border border-indigo-600 p-10 mr-1 ">
+                {/* Right Section - Text (3/4 on xl, full width on others) */}
+                <div className="w-full xl:w-3/4 border border-indigo-600 p-10">
                     <h2 className="text-xl font-semibold">hekkko</h2>
                 </div>
             </div>
