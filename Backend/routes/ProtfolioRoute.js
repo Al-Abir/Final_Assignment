77f425fa506d7d76db982ada6c6d543f16b35462
Al-Abir <sheikhabir196@gmail.com>
2025-02-06 11:15:40 +0600

    Backend

--- Backend/routes/ProtfolioRoute.js
+++ Backend/routes/ProtfolioRoute.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const { Intro, About } = require('../Models/ProtfolioModel');
+const service = require('../Models/Sevice')
+const blog = require('../Models/Blog')
+const team = require('../Models/Team')
 
 router.get('/portfolio-data', async (req, res) => {
    try {
-     const intros = await Intro.find()
+     const intros = await Intro.find();
      const abouts = await About.find();
+     const services = await service.find();
+     const teams =   await team.find();
+     const blogs = await blog.find();
 
      res.status(200).send({
       intro: intros[0],
-      about: abouts[0]
+      about: abouts[0],
+      service: services[0],
+      team : teams[0],
+      blog: blogs[0]
+
      })
 
     
--- Backend/server.js
+++ Backend/server.js
@@ -1,42 +1,42 @@
-const express = require('express');
-const path = require('path');
-const dotenv = require('dotenv')
-
-const portfolioRoutes = require('./routes/ProtfolioRoute');
-const connectDB = require('./config/dbConfig');
-
-
-
-//configure env
-dotenv.config()
-
-//database Confiq
-connectDB();
-
-
-
-
-
-const app = express();
-
-// Connect to MongoDB
-
-// Middleware
-app.use(express.json());
-
-// API Routes
-app.use('/api/v1', portfolioRoutes);
-
-// Serve static files for the frontend
-app.use(express.static(path.join(__dirname, '../client', 'dist')));
-
-// Catch-all route for serving the React app
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
-});
-
-// Start server
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Server listening on Port ${port}`);
-});
+     const express = require('express');
+     const path = require('path');
+     const dotenv = require('dotenv')
+     
+     const portfolioRoutes = require('./routes/ProtfolioRoute');
+     const connectDB = require('./config/dbConfig');
+     
+     
+     //configure env
+     dotenv.config()
+     
+     //database Confiq
+     connectDB();
+     
+     
+     
+     
+     
+     const app = express();
+     
+     // Connect to MongoDB
+     
+     // Middleware
+     app.use(express.json());
+     
+     // API Routes
+     app.use('/api/v1', portfolioRoutes);
+     
+     // Serve static files for the frontend
+     app.use(express.static(path.join(__dirname, '../client', 'dist')));
+     
+     // Catch-all route for serving the React app
+     app.get('*', (req, res) => {
+         res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
+     });
+     
+     // Start server
+     const port = process.env.PORT || 5000;
+     app.listen(port, () => {
+         console.log(`Server listening on Port ${port}`);
+     });
+     
